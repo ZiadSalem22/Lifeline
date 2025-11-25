0e2cdc93780e53c0510ecebba374a3b2f5b9d037
z.salem <z.salem@ggw.com>
2025-11-25 16:13:58 -0500

    chore: remove deployment artifacts (temp packages, zips, and service principal files)

--- backend-deploy-temp/jest.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  testEnvironment: 'node',
-  roots: ['<rootDir>/src', '<rootDir>/test'],
-  transform: {
-    '^.+\\.jsx?$': 'babel-jest',
-  },
-};
--- backend-deploy-temp/src/domain/Tag.js
+++
@@ -1,9 +0,0 @@
-class Tag {
-    constructor(id, name, color) {
-        this.id = id;
-        this.name = name;
-        this.color = color;
-    }
-}
-
-module.exports = Tag;
