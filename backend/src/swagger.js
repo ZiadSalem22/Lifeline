8c9356eedff20679b251b275161b8a1e9bbbbaa2
z.salem <z.salem@ggw.com>
2025-11-25 15:47:07 -0500

    swagger: serve JSON endpoint before UI middleware to avoid HTML being returned for the spec

--- backend/src/swagger.js
+++ backend/src/swagger.js
@@ -2,9 +2,6 @@ const swaggerUi = require('swagger-ui-express');
 const baseSpec = require('../swagger.json');
 
 module.exports = function (app) {
-  // Serve Swagger UI at /api-docs and instruct it to fetch the spec from /api-docs/swagger.json
-  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(null, { swaggerUrl: '/api-docs/swagger.json' }));
-
   // Serve a dynamic swagger.json that uses the current request host/protocol as server URL.
   app.get('/api-docs/swagger.json', (req, res) => {
     try {
@@ -19,4 +16,7 @@ module.exports = function (app) {
       res.status(500).json({ error: 'Unable to load swagger spec' });
     }
   });
+
+  // Serve Swagger UI at /api-docs and instruct it to fetch the spec from /api-docs/swagger.json
+  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(null, { swaggerUrl: '/api-docs/swagger.json' }));
 };
